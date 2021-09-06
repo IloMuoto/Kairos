4c1f9dac4e36e6617ce2305801a600b687381c59
Jason Stallings <jason@stallin.gs>
2021-09-05 21:01:50 -0500

    cosmwasm cleanup

--- cosmwasm/src/contract.rs
+++ cosmwasm/src/contract.rs
@@ -37,9 +37,6 @@ pub fn try_submit<S: Storage, A: Api, Q: Querier>(
     env: Env,
     body: String,
 ) -> StdResult<HandleResponse> {
-    let sender_address_raw = deps.api.canonical_address(&env.message.sender)?;
-    // let state = config_read(&deps.storage).load()?;
-
     config(&mut deps.storage).update(|mut state| {
         state.count += 1;
         state.requests.push(body);
@@ -86,105 +83,4 @@ fn query_request<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>, index: 
     let requests = state.requests.clone();
 
     return Ok(RequestResponse { id: index, body: requests[index].clone() })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env};
-    use cosmwasm_std::{coins, from_binary, StdError};
-
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies(20, &[]);
-
-        let msg = InitMsg { count: 17 };
-        let env = mock_env("creator", &coins(1000, "earth"));
-
-        // we can just call .unwrap() to assert this was a success
-        let res = init(&mut deps, env, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(&deps, QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
-    }
-
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies(20, &coins(2, "token"));
-
-        let msg = InitMsg { count: 17 };
-        let env = mock_env("creator", &coins(2, "token"));
-        let _res = init(&mut deps, env, msg).unwrap();
-
-        // beneficiary can release it
-        let env = mock_env("anyone", &coins(2, "token"));
-        let sender = env.clone().message.sender;
-        let msg = HandleMsg::Increment {};
-        let _res = handle(&mut deps, env, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(&deps, QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-
-        let res = query(&deps, QueryMsg::GetPlace { address: sender }).unwrap();
-        let value: PlaceResponse = from_binary(&res).unwrap();
-        assert_eq!(1, value.place);
-    }
-
-    #[test]
-    fn increment_twice() {
-        let mut deps = mock_dependencies(20, &coins(2, "token"));
-
-        let msg = InitMsg { count: 0 };
-        let env = mock_env("creator", &coins(2, "token"));
-        let _res = init(&mut deps, env, msg).unwrap();
-
-        // beneficiary can release it
-        let env = mock_env("anyone", &coins(2, "token"));
-        let msg = HandleMsg::Increment {};
-        let _res = handle(&mut deps, env, msg).unwrap();
-
-        // This should fail to increment.
-        let env2 = mock_env("anyone", &coins(2, "token"));
-        let msg2 = HandleMsg::Increment {};
-        // TODO: It would be nice to assert on the error.
-        let _res2 = handle(&mut deps, env2, msg2);
-
-        // should increase counter by 1
-        let res = query(&deps, QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(1, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies(20, &coins(2, "token"));
-
-        let msg = InitMsg { count: 17 };
-        let env = mock_env("creator", &coins(2, "token"));
-        let _res = init(&mut deps, env, msg).unwrap();
-
-        // beneficiary can release it
-        let unauth_env = mock_env("anyone", &coins(2, "token"));
-        let msg = HandleMsg::Reset { count: 5 };
-        let res = handle(&mut deps, unauth_env, msg);
-        match res {
-            Err(StdError::Unauthorized { .. }) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
-
-        // only the original creator can reset the counter
-        let auth_env = mock_env("creator", &coins(2, "token"));
-        let msg = HandleMsg::Reset { count: 5 };
-        let _res = handle(&mut deps, auth_env, msg).unwrap();
-
-        // should now be 5
-        let res = query(&deps, QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
-    }
 }
--- cosmwasm/submit.sh
+++ cosmwasm/submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-terracli tx wasm execute terra1hqrdl6wstt8qzshwc6mrumpjk9338k0l93hqyd '{"submit":{"body":"escaped json"}}' --from test1 --chain-id=localterra --gas=auto --fees=1000000uluna --broadcast-mode=block
+terracli tx wasm execute terra1hqrdl6wstt8qzshwc6mrumpjk9338k0l93hqyd '{"submit":{"body":"{\"name\":\"Test Company\",\"members\":[\"Jason Stallings\"]}"}}' --from test1 --chain-id=localterra --gas=auto --fees=1000000uluna --broadcast-mode=block
